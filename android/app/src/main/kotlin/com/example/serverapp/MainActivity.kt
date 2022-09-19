b6a027f85c09f571e4beee6cc6c0f766ecef6960
aminelinux <aminehamdi1992@gmail.com>
2022-09-19 10:40:34 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/serverapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.serverapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+List<ChatClient> clients = [];
+void main() {
+  ServerSocket socket;
+  ServerSocket.bind('127.0.0.1', 9000).then((ServerSocket server) {
+    socket = server;
+    server.listen(handleClient);
+  });
+}
+
+void handleClient(Socket client) {
+  print('Connection from '
+      '${client.remoteAddress.address}:${client.remotePort}');
+
+  clients.add(ChatClient(client));
+
+  client.write("Welcome Client App!!! "
+      "There are ${clients.length - 1} other clients\n");
+}
+
+class ChatClient {
+  late Socket _socket;
+  late String _address;
+  late int _port;
+
+  ChatClient(Socket s) {
+    _socket = s;
+    _address = _socket.remoteAddress.address;
+    _port = _socket.remotePort;
+
+    _socket.listen(messageHandler,
+        onError: errorHandler, onDone: finishedHandler);
+  }
+
+  void messageHandler(List<int> data) {
+    String message = String.fromCharCodes(data).trim();
+    distributeMessage(this, '${_address}:${_port} Message: $message');
+  }
+
+  void errorHandler(error) {
+    print('${_address}:${_port} Error: $error');
+    removeClient(this);
+    _socket.close();
+  }
+
+  void finishedHandler() {
+    print('${_address}:${_port} Disconnected');
+    removeClient(this);
+    _socket.close();
+  }
+
+  void write(String message) {
+    _socket.write(message);
+  }
+
+  void distributeMessage(ChatClient client, String message) {
+    for (ChatClient c in clients) {
+      if (c != client) {
+        c.write(message + "\n");
+      }
+    }
+  }
+
+  void handleConnection(Socket client) {
+    print('Connection from '
+        '${client.remoteAddress.address}:${client.remotePort}');
+
+    clients.add(ChatClient(client));
+
+    client.write("Welcome to dart-chat! "
+        "There are ${clients.length - 1} other clients\n");
+  }
+
+  void removeClient(ChatClient client) {
+    clients.remove(client);
+  }
+}
